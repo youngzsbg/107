59cfb06ffbc94c651f06f1c6e078395fab9808ef
youngzsbg <k-payne101@hotmail.com>
2024-11-09 14:50:46 -0500

    put post patch delete in server

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import json
 
 app = Flask(__name__)
@@ -32,5 +32,59 @@ def greet(name):
 def farewell(name):
     return f"""
 <h1 style=color:blue>Get From Yah {name}!</h1>"""
+
+
+################################
+
+#get
+
+products = []
+@app.get("/api/products")
+def get_products():
+    return json.dumps(products)
+
+#put
+
+@app.post("/api/products")
+def save_products():
+    item = request.get_json()
+    print(item)
+    products.append(item)
+    return json.dumps(item)
+
+
+#put
+
+@app.put("/api/products/<int:index>")
+def update_products(index):
+    updated_item=request.get_json()
+    if 0<= index <=len(products):
+        products[index] = updated_item
+        return json.dumps(updated_item)
+    else:
+        return "That index does not exist"
+    
+#delete
+
+@app.delete("/api/products/<int:index>")
+def delete_products(index):
+    delete_item = request.get_json()
+    if 0<= index <=len(products):
+        delete_item = products.pop(index)
+        return json.dumps(delete_item)
+    else:
+        return "That index does not exist"
+
+# patch the method to update a specific element into python is: list.update
+
+@app.patch("/api/products/<int:index>")
+def patch_products(index):
+    updated_field = request.get_json()
+    if 0<= index <=len(products):
+        updated_field(index).update(updated_field)
+        return json.dumps(updated_field)
+    else:
+        return "That index does not exist"
+
 
 app.run(debug=True)
